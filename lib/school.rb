7807ffa5cafe6f934a7323e14e6f87f39f1483d7
bstizzle <whereyouben@gmail.com>
2020-10-27 17:52:40 -0400

    created school class, which has methods to fill out and retrieve info from a HoA of the grade levels and students

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
+
 # code here!
+class School
+    def initialize(name, roster={})
+        @name = name 
+        @roster = roster
+    end 
+
+    attr_accessor :name, :roster
+
+    def add_student(name, grade)
+        if @roster.has_key?(grade)
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end 
+
+    def grade(year)
+        @roster[year]
+    end 
+
+    def sort 
+        @roster.each do |grade, students|
+            students.sort!
+        end 
+    end 
+
+end 
